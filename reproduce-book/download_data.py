cbc0fc60ef05e869a55e5ac2b4a41cbd6bea86e9
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-10-24 09:45:28 +0200

    Add correct suffix to data

--- reproduce-book/download_data.py
+++ reproduce-book/download_data.py
@@ -7,8 +7,8 @@ from config import MRI2FEMDATADIR, DATADIR
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    download_data.download_mri2fem_dataset(MRI2FEMDATADIR)
-    download_data.download_extra_data(DATADIR)
+    download_data.download_mri2fem_dataset(MRI2FEMDATADIR.with_suffix(".tar.gz"))
+    download_data.download_extra_data(DATADIR.with_suffix(".tar.gz"))
 
 
 if __name__ == "__main__":
